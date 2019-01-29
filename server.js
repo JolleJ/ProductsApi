334678ab9306c9863fda669b1098f17861a90d15
JolleJ <joeljansson96@gmail.com>
2019-01-29 03:16:17 +0200

    Connecting to server

--- server.js
+++ server.js
@@ -3,13 +3,6 @@ const express = require('express');
 const Client  = require('pg');
 const bodyParser = require('body-parser');
 
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-});
-
-client.connect();
-
 const app = express();
 const port = process.env.PORT || 8080;
 
@@ -19,13 +12,7 @@ app.use(bodyParser.json());
 var router = express.Router();
 
 router.get('/', function(req,res){
-    client.query('SELECT * FROM product_table;', (err, res) => {
-        if (err) throw err;
-        for (let row of res.rows) {
-          console.log(JSON.stringify(row));
-        }
-        client.end();
-      });
+    res.statusCode(200);
 });
 
 app.use('/api', router);
