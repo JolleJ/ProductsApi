fd644be347d376869e774b8701fec3ff9e12f98b
JolleJ <joeljansson96@gmail.com>
2019-01-29 03:10:59 +0200

    Connecting to server

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 
 const express = require('express');
-const { Pool, Client } = require('pg');
+const Client  = require('pg');
 const bodyParser = require('body-parser');
 
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+
+client.connect();
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -11,21 +17,8 @@ app.use(bodyParser.json());
 
 var router = express.Router();
 
-const pool = new Pool({
-    user: "otvuzfjqceqoom",
-    host: "ec2-54-75-245-94.eu-west-1.compute.amazonaws.com",
-    database: "d6h6dl284lgq98",
-    password: "ac7b563a023c14422324bad89ab77cb81249f2e31a6c2b0ee186537cce346304",
-    port: process.env.PORT,
-});
-
-
-
 router.get('/', function(req,res){
-    pool.query('SELECT NOW()', (err, res) => {
-        console.log(err, res)
-        pool.end()
-      })
+    res.status(200);
 });
 
 app.use('/api', router);
