098cf69e0a9219b49a83b8d3b93930b11118ba98
JolleJ <joeljansson96@gmail.com>
2019-01-29 15:46:17 +0200

    Checking status

--- queries.js
+++ queries.js
@@ -1,14 +1,15 @@
-const Pool = require('pg').Pool;
-const pool = new Pool({
-    user: 'otvuzfjqceqoom',
-    host: 'ec2-54-75-245-94.eu-west-1.compute.amazonaws.com',
-    databse: 'd6h6dl284lgq98',
-    password: 'ac7b563a023c14422324bad89ab77cb81249f2e31a6c2b0ee186537cce346304',
-    port: 5432,
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
 });
 
+client.connect();
+
+
 const getProducts = (req, res) => {
-    pool.query('SELECT * FROM product_table', (error, results) =>{
+    client.query('SELECT * FROM product_table', (error, results) =>{
         if(error) {
             throw error;
         }
