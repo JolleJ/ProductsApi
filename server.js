98cbb6020ae17e031ca6c4aadf38edb924ef6026
JolleJ <joeljansson96@gmail.com>
2019-01-29 23:59:40 +0200

    Test

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ app.use(
 
 app.use(bodyParser.json());
 
-
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', (req, res) => {
     res.json({info: "Server Working!"});
