54eed98a7eb06e6f0b822ac49cc1c596bc9bc6c1
JolleJ <joeljansson96@gmail.com>
2019-01-29 02:33:52 +0200

    First routing

---
+++ controller.js
@@ -0,0 +1,5 @@
+module.exports = {
+    status : function(req, res){
+        res.status(200);
+    }
+}
--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require('express');
 const client = require('pg');
 const bodyParser = require('body-parser');
 
+
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log("We are live on ${port}/");
+    console.log("We are live!");
+});
+
+app.get('status', function(req, res){
+    res.status(200);
 });
