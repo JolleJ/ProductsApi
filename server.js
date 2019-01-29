3b82eb304d1db34857163a87f1088d86f2752d76
JolleJ <joeljansson96@gmail.com>
2019-01-29 03:03:08 +0200

    Connecting to server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const { Pool, Client } = require('pg');
 const bodyParser = require('body-parser');
@@ -10,7 +11,13 @@ app.use(bodyParser.json());
 
 var router = express.Router();
 
-const pool = new Pool();
+const pool = new Pool({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PORT,
+});
 
 
 
